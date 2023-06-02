36078c1c3cc0394a14d4c3d572273f9b0b7db8bf
GhulamAkber368 <GhulamAkber368@gmail.com>
2023-06-02 09:45:06 -0700

    Unit Testing

---
+++ lib/counter_controller.dart
@@ -0,0 +1,16 @@
+class Counter {
+  int _count = 0;
+  int get count => _count;
+
+  void incrementCounter() {
+    _count++;
+  }
+
+  void decrementCounter() {
+    _count--;
+  }
+
+  void reset() {
+    _count = 0;
+  }
+}
